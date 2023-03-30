7930c164e0a383831171166e8a5734d586f19f66
mgngm <mgngm@amazon.com>
2023-03-30 10:21:43 +0200

    test

---
+++ nextjs-amplified-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "monorepo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
---
+++ nextjs-amplified-app/pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+
+import { Amplify, API } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
